7b0f5bbb6da713856762db8a8bca3d0e6f650074
Jay Vanny Orabao <your-github-email@example.com>
2026-06-25 13:15:59 +0800

    Set up Android Capacitor debug build

---
+++ android/app/src/main/java/ph/gov/dilg/region10/pms10/MainActivity.java
@@ -0,0 +1,5 @@
+package ph.gov.dilg.region10.pms10;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.gov.dilg.region10.pms10',
+  appName: 'PMS10',
+  webDir: 'dist'
+};
+
+export default config;
