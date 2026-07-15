f5686d2a2affd8fa45fe37fe05afd1dec95b794c
Jay Vanny Orabao <your-github-email@example.com>
2026-07-15 15:01:35 +0800

    Rename Capacitor app to PMS10

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'ph.gov.dilg.region10.pms10',
-  appName: 'MERO¹⁰',
+  appName: 'PMS10',
   webDir: 'dist',
 }
 
