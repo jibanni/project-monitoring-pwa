4cb7c02b303422fa0fbaadb5861cd36bfc7a99e0
Jay Vanny Orabao <your-github-email@example.com>
2026-07-21 13:44:12 +0800

    Clean up mobile navbar responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { initPms10BottomNavRuntimeFix } from "./utils/pms10BottomNavRuntimeFix";
-import { initPms10BottomNavLevelOverride } from "./utils/pms10BottomNavLevelOverride";
 import { initPms10DisableZoomExceptMap } from "./utils/disableAppZoomExceptMap";
 import './utils/fitProjectUpdateHeroTitle'
 import { StrictMode } from 'react'
@@ -10,16 +8,11 @@ import './styles/layout.css'
 import './styles/dashboard.css'
 import App from './App.tsx'
 import './utils/titleCaseLocationsDom'
-import { initPms10ProgramDropdownCaps } from './utils/pms10ProgramDropdownCaps'
 import { initPms10StandalonePwaClass } from './utils/pms10StandalonePwaClass'
-import { initPms10BottomNavLock } from './utils/pms10BottomNavLock'
-
-initPms10BottomNavRuntimeFix();
-initPms10BottomNavLevelOverride();
+import { initPms10ProgramDropdownCaps } from './utils/pms10ProgramDropdownCaps'
 initPms10DisableZoomExceptMap();
-initPms10ProgramDropdownCaps()
 initPms10StandalonePwaClass()
-initPms10BottomNavLock()
+initPms10ProgramDropdownCaps()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
