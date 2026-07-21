bea88fcc6d9ccdb05ca1e313f52adde5b3d1de76
Jay Vanny Orabao <your-github-email@example.com>
2026-07-21 11:38:22 +0800

    Enable program dropdown uppercase guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { initPms10BottomNavRuntimeFix } from "./utils/pms10BottomNavRuntimeFix";
 import { initPms10BottomNavLevelOverride } from "./utils/pms10BottomNavLevelOverride";
 import { initPms10DisableZoomExceptMap } from "./utils/disableAppZoomExceptMap";
-import { initPms10ProgramDropdownCaps } from "./utils/pms10ProgramDropdownCaps";
 import './utils/fitProjectUpdateHeroTitle'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -11,11 +10,12 @@ import './styles/layout.css'
 import './styles/dashboard.css'
 import App from './App.tsx'
 import './utils/titleCaseLocationsDom'
+import { initPms10ProgramDropdownCaps } from './utils/pms10ProgramDropdownCaps'
 
 initPms10BottomNavRuntimeFix();
 initPms10BottomNavLevelOverride();
 initPms10DisableZoomExceptMap();
-initPms10ProgramDropdownCaps();
+initPms10ProgramDropdownCaps()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
